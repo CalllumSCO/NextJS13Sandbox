37bb425d8c94fab911241af72a923f70eeda3809
Callum <callum.mackinnon@btinternet.com>
2023-09-28 16:46:44 +0100

    Created a custom loading screen

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import TicketList from './TicketList'
+import Loading from './loading'
 
 export default function page() {
   return (
     <main>
-        <nav>
-          <div>
-            <h2>Tickets</h2>
-            <p><small>Cuurently open tickets:</small></p>
-          </div>
-        </nav>
+      <nav>
+        <div>
+          <h2>Tickets</h2>
+          <p><small>Cuurently open tickets:</small></p>
+        </div>
+      </nav>
 
+      <Suspense fallback={<Loading />}>
         <TicketList />
+      </Suspense>
     </main>
   )
 }
