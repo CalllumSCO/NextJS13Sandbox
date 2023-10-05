4b43b8016315acfc9ee5445a1ca5d09b4c934dd1
Callum <callum.mackinnon@btinternet.com>
2023-10-05 16:02:57 +0100

    Added Authentication Software using Supabase

--- app/(auth)/login/page.jsx
+++ app/(auth)/login/page.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+"use client"
+
+import { useState } from 'react'
+import AuthForm from '../AuthForm'
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs"
+import { useRouter } from 'next/navigation';
+
 
 export default function Login() {
+    const [error, setError] = useState();
+    const router = useRouter();
+
+    const handleSubmit = async (e, email, password) => {
+        e.preventDefault()
+        console.log(email, password)
+
+        const supabase = createClientComponentClient();
+        const { error } = await supabase.auth.signInWithPassword({
+            email,
+            password
+        })
+        if (error) {
+            setError(error.message);
+        } else {
+            router.push('/')
+        }
+    }
     return (
-        <h2 className="text-center">Log In</h2>
+        <>
+            <h2 className="text-center">Log In</h2>
+            <AuthForm handleSubmit={handleSubmit} />
+
+            {error && (
+                <div className='error'>{error}</div>
+            )}
+        </>
     )
 }
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,20 +2,23 @@ import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import Logo from './dojo-logo.png'
+import LogoutButton from './LogoutButton';
 
-export default function Navbar() {
+export default function Navbar({ user }) {
   return (
     <nav>
-        <Image
-            src={Logo}
-            alt="Dojo Helpdesk Logo"
-            width={70}
-            quality={100}
-            placeholder='blur'
-        />
-        <h1>Dojo Helpdesk</h1>
-        <Link href="/">Dashboard</Link>
-        <Link href="/tickets">Tickets</Link>
+      <Image
+        src={Logo}
+        alt="Dojo Helpdesk Logo"
+        width={70}
+        quality={100}
+        placeholder='blur'
+      />
+      <h1>Dojo Helpdesk</h1>
+      <Link href="/">Dashboard</Link>
+      <Link href="/tickets" className='mr-auto'>Tickets</Link>
+      {user && <span>Hello, {user.email} </span>}
+      <LogoutButton />
     </nav>
   )
 }
